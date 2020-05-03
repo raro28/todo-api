2cc91fb4d5c95c7e7b95a6f1ce57d2aff74ad014
Hector Diaz <3kthor3adward@gmail.com>
2020-05-03 18:51:38 -0500

    cors

--- src/main/java/mx/ekthor/todo/rest/configuration/WebMvcConfiguration.java
+++ src/main/java/mx/ekthor/todo/rest/configuration/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package mx.ekthor.todo.rest.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setUseTrailingSlashMatch(true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
